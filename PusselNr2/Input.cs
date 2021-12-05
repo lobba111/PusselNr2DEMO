d8bb0a287b93a21f4276411cb8837acdf3a42a98
lobba111 <robinolsson@gmail.com>
2021-12-05 02:44:01 +0100

    Add project files.

---
+++ PusselNr2/Input.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PusselNr2
+{
+    public class Input
+    {
+        public int Range { get; set; }
+        public string Movement { get; set; }
+        public Input(int range, string movement)
+        {
+            Range = range;
+            Movement = movement;
+        }
+        
+    }
+}
---
+++ PusselNr2/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PusselNr2;
+using System.Collections;
+using System.Text.RegularExpressions;
+
+int forward = 0;
+int depth = 0;
+int up = 0; 
+int down = 0;
+Regex regex = new Regex(@"^\d$");
+string[] inputData = System.IO.File.ReadAllLines(@"C:\Users\robin\OneDrive\Skrivbord\inputdata.txt");
+List<int> filterdDataFromInputValues = new List<int>(); 
+List<string> filterdDataCommandos = new List<string>();
+
+
+foreach (string input in inputData)
+{
+    var filterdNumber = Regex.Match(input, @"\d+").Value;
+    filterdDataFromInputValues.Add(Int32.Parse(filterdNumber));// okej så här har jag lyckats att filtrea min inputdata och tagit ut mina nummer
+    string[] hej = Regex.Split(input, @"\d+");//okej nu har jag lyckats filtrera orden och sparar dom i sen separat lista
+    filterdDataCommandos.Add(hej[0]);
+}
+foreach (var data in filterdDataFromInputValues)
+{
+    Console.WriteLine(data);
+}
+foreach (string data in filterdDataCommandos)
+{
+    Console.WriteLine(data);
+}
+
+
+
