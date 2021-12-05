02d5266e38c8d2f671175004720329567e3c6d6b
lobba111 <robinolsson@gmail.com>
2021-12-05 02:52:44 +0100

    Byte namn och städa lite

--- PusselNr2/Program.cs
+++ PusselNr2/Program.cs
@@ -7,7 +7,6 @@ int forward = 0;
 int depth = 0;
 int up = 0; 
 int down = 0;
-Regex regex = new Regex(@"^\d$");
 string[] inputData = System.IO.File.ReadAllLines(@"C:\Users\robin\OneDrive\Skrivbord\inputdata.txt");
 List<int> filterdDataFromInputValues = new List<int>(); 
 List<string> filterdDataCommandos = new List<string>();
@@ -17,8 +16,8 @@ foreach (string input in inputData)
 {
     var filterdNumber = Regex.Match(input, @"\d+").Value;
     filterdDataFromInputValues.Add(Int32.Parse(filterdNumber));// okej så här har jag lyckats att filtrea min inputdata och tagit ut mina nummer
-    string[] hej = Regex.Split(input, @"\d+");//okej nu har jag lyckats filtrera orden och sparar dom i sen separat lista
-    filterdDataCommandos.Add(hej[0]);
+    string[] wordCommando = Regex.Split(input, @"\d+");//okej nu har jag lyckats filtrera orden och sparar dom i sen separat lista
+    filterdDataCommandos.Add(wordCommando[0]);
 }
 foreach (var data in filterdDataFromInputValues)
 {
